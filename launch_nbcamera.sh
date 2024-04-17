ca090638360af703a7ed4b6984895cb270261597
turb0d0g <69019361+turb0d0g@users.noreply.github.com>
2024-04-17 15:17:00 -0500

    Create launch_nbcamera.sh

---
+++ launch_nbcamera.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# launch_nbcamera.sh
+# navigate to home directory, then to this directory, then execute python script, then back home
+
+cd /home/pi/Naturebytes/Scripts/
+python3 nbcamera.py
