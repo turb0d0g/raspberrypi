27d01739089d5d3eb7c22e63eb73556da318bd64
turb0d0g <69019361+turb0d0g@users.noreply.github.com>
2024-04-17 15:07:22 -0500

    Create findPi.sh

---
+++ findPi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+#Very useful in getting the ip address of my raspberry pi. In case thats not the
+#point, I added the option to pull IPs of all devices on network.
+
+function raspPiFinder {
+    if [ "${1}" == "pi" ]; then
+        sudo nmap -sP 192.168.1.148/24 | awk '/^Nmap/{ip=$NF}/B8:27:EB/{print ip}'
+
+    else
+        sudo nmap -sP 192.168.1.148/24
+
+    fi
+}
+
+raspPiFinder $1
+
+
